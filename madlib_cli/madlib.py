6a242d98ab71bdb8e4c9b5ea6433a5ebd28f38f9
dchoi294 <dchoi294@gmail.com>
2023-01-02 16:23:44 -0800

    fixing

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -41,14 +41,22 @@ intro = """
 
 def main():
     print(intro)
-    file = read_template("assets/madlib.txt")
-    string, parts = parse_template(file)
-    filled = []
-    for i in parts:
-        print(i)
-        user_input = input("> ")
-        filled.append(user_input)
-    result = merge(string, filled)
+    file_path = input(" Enter File Path or hit 'Enter' for the default > ")
+    if file_path == "":
+        file_path = "assets/madlib.txt"
+    try:
+        script = read_template(file_path)
+        empty_string, parts = parse_template(script)
+        filled_list = []
+        for i in parts:
+            user_input = input(f"  Enter {i} > ")
+            filled_list.append(user_input)
+        result = merge(empty_string, filled_list)
+        print(f"\nHere is your Madlib:\n\n" + result)
+        with open('assets/result.txt', 'w') as writer:
+            writer.write(result)
+    except:
+        print('An error occurred')
 
 
 if __name__ == "__main__":
